a262f1925a5bfb2b6b9de8cf2bf72c841dc91ae6
Tyler Nguyen <tynguyen06@gmail.com>
2017-07-19 00:22:45 -0700

    remove life component method

--- src/Components/Life/Life.js
+++ src/Components/Life/Life.js
@@ -1,5 +1,6 @@
 import React from "react";
 class Life extends React.Component{
+    //1st
     constructor(props){
         super(props);
         this.state = {count: 10};
@@ -7,37 +8,55 @@ class Life extends React.Component{
         console.log("1. This is the constructor method. Always first");
         // If we need to define state, this should always happen in the constructor
     }
-    componentWillUpdate(nextProps, nextState){
-      console.log("4. This runs right before the html is re-rendered, in this case right before state changes")
-      if(this.renderOrder == 2){this.renderOrder = 5}
+
+    //2nd
+    render(){
+      console.log(`${this.renderOrder}. This is the render method. Invoked after constructor and before state changes`);
+      return (
+        <h1>COUNT: {this.state.count}</h1>
+      )
     }
-    componentDidUpdate(prevProps, prevState){
-        console.log("6. This runs right after the html is re-rendered, in this case right after state changes")
-        if(this.state.count <= 0){
-            clearInterval(this.countDown);
-            console.log("7. Countdown Finished and state has interval has been stopped")
-        }
-    }
+
+    //3rd
     componentDidMount(){
         console.log("3. componentDidMount method. Invoked immediataly after the first render runs.");
         // setTimeout(() => {
         //   this.setState({count: this.state.count - 1})
         // }, 1000)
         this.countDown = setInterval(()=>{
+            //State is changed every second, so compenent re-renders accordingly
             this.setState({count: this.state.count - 1})
-            // console.log('setInterval running');
-            // if(this.state.count == 0) {
-            //   clearInterval(this.countDown)
-            // }
-
+            console.log('state has changed');
         }, 1000)
         // console.log('this.counter',this.countDown)
     }
-    render(){
-        console.log(`${this.renderOrder}. This is the render method. Invoked after constructor and before state changes`);
-        return (
-            <h1>COUNT: {this.state.count}</h1>
-        )
+
+    //4th
+    componentWillUpdate(nextProps, nextState){
+      console.log('nextProps', nextProps)
+      console.log('nextState', nextState)
+      console.log("4. This runs right before the html is re-rendered, in this case right before state changes")
+      if(this.renderOrder == 2){this.renderOrder = 5}
+    }
+    componentDidUpdate(prevProps, prevState){
+      console.log("6. This runs right after the html is re-rendered, in this case right after state changes")
+      if(this.state.count <= 0 && prevState.count > 0){
+          clearInterval(this.countDown);
+          console.log("7. Countdown Finished and state has interval has been stopped");
+          // Let's make a mess to be cleaned up by componentWillUnmount
+          this.h1 = document.createElement('h1');
+          this.h1.innerText = "Making A Mess"
+          document.body.appendChild(this.h1)
+          // Runs this.props.destroyMethod after 3 seconds
+          setTimeout(() => {
+              console.log("Invoking this.props.destroyMethod()")
+              this.props.destroyMethod();
+          }, 3000)
+      }
+    }
+    componentWillUnmount(){
+      console.log("7. This runs right before the component is removed from the DOM");
+      document.body.removeChild(this.h1)
     }
 }
 export default Life;
--- src/app.js
+++ src/app.js
@@ -6,16 +6,32 @@ import Counter from './Components/Counter/Counter.js';
 import Life from './Components/Life/Life.js';
 
 let user = {first_name: "Steph", last_name: "Villalobos", age: 30};
-
+//let parent, App, mount/remove it's component and their respective elements/events that were created
 class App extends React.Component {
+  constructor(props){
+       super(props);
+       this.state = {showCountDown: true}
+   }
+   removeCountDown = () => {
+     this.setState({showCountDown: false});
+   }
   render() {
-    return (
-        // <Prop user={user}/>
-        // <Event/>
-        // <State/>
-        // <Counter name="Tyler"/>
-        <Life/>
-    )
+    // return (
+    //     <Prop user={user}/>
+    //     <Event/>
+    //     <State/>
+    //     <Counter name="Tyler"/>
+    //     <Life/>
+    // )
+    if(this.state.showCountDown){
+       return (
+           <Life destroyMethod={this.removeCountDown}/>
+       )
+    } else {
+         return (
+             <h1>Crisis Averted</h1>
+         )
+    }
   }
 }
 
